8ba10358483439276492e52f67f5cf946bcb635b
Christian Hartung <christian.hartung@olist.com>
2025-10-23 15:17:09 -0300

    feat: add async version to reduce context-switching

--- src/django_whatsapp/views.py
+++ src/django_whatsapp/views.py
@@ -16,9 +16,27 @@ from .utils import verify_request
 class WebhookView(View):
     """View to handle WhatsApp webhook requests."""
 
-    http_method_names: ClassVar[list[str]] = ["get", "post", "options"]
+    http_method_names: list[str] = ["get", "post", "options"]  # noqa: RUF012
 
     def get(self, request: HttpRequest) -> HttpResponse:
+        """Handle a WhatsApp Verification Request."""
+        try:
+            return verify_request(request, verify_token=settings.WHATSAPP_VERIFY_TOKEN)
+        except SuspiciousOperation:
+            return HttpResponseBadRequest()
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        """Handle a WhatsApp Event Notification."""
+        raise NotImplementedError
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AsyncWebhookView(View):
+    """View to handle WhatsApp webhook requests."""
+
+    http_method_names: list[str] = ["get", "post", "options"]  # noqa: RUF012
+
+    async def get(self, request: HttpRequest) -> HttpResponse:
         """Handle a WhatsApp Verification Request."""
         try:
             return verify_request(request, verify_token=settings.WHATSAPP_VERIFY_TOKEN)
